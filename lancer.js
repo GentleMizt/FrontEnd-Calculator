fac193d258f4f7ce6bed3d4743bd923824091e3c
GentleMizt <olamideiyanda18@gmail.com>
2023-02-27 00:33:34 +0100

    applying js

--- lancer.js
+++ lancer.js
@@ -1,23 +1,37 @@
-// Theme Switching Buttons
+    // Theme Switching Buttons
 
-const btn1 = document.getElementById('btn1'); //theme1 button
-const btn2 = document.getElementById('btn2'); //theme2 button
-const btn3 = document.getElementById('btn3'); //theme3 button
+    const btn1 = document.getElementById('btn1'); //theme1 button
+    const btn2 = document.getElementById('btn2'); //theme2 button
+    const btn3 = document.getElementById('btn3'); //theme3 button
 
-// Function that allows the button to change the theme
-btn1.addEventListener('click', ()=>{
-    document.getElementById('themes').href = 'css/theme1.css'
-})
+    // Function that allows the button to change the theme
+    btn1.addEventListener('click', ()=>{
+        document.getElementById('themes').href = 'css/theme1.css'
+    })
 
-btn2.addEventListener('click', ()=>{
-    document.getElementById('themes').href = 'css/theme2.css'
-})
+    btn2.addEventListener('click', ()=>{
+        document.getElementById('themes').href = 'css/theme2.css'
+    })
 
-btn3.addEventListener('click', ()=>{
-    document.getElementById('themes').href = 'css/theme3.css'
-})
+    btn3.addEventListener('click', ()=>{
+        document.getElementById('themes').href = 'css/theme3.css'
+    })
 
-// Calculations and Implementations.
-const display = document.getElementById('display');
+    // Calculations and Implementations.
+    const display = document.getElementById('display');
 
-const buttons = Array.from(document.getElementsByClassName('button'));
+    const buttons = Array.from(document.getElementsByClassName('button'));
+
+    buttons.map( button =>{
+        button.addEventListener('click',calculate)
+    });
+    
+    function calculate(e){
+        switch (e.target.innerText) {
+            case 'RESET':
+                display.innerText = '';
+                break
+            default:
+                display.innerText += e.target.innerText;
+        }
+    }
